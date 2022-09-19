17939ada3786bf0ad3a61ec6b0b53d81bb3ab1c9
PickN1ck <oopsparanoia@gmail.com>
2022-09-18 19:06:17 -0600

    CORS back

--- modules/core/cors.js
+++ modules/core/cors.js
@@ -1,30 +1,30 @@
-// function cors(app) {
-//   app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header(
-//       'Access-Control-Allow-Headers',
-//       'Origin, X-Requested-With, Control-Type, Accept, Authorization',
-//     );
-//     if (req.method === 'OPTIONS') {
-//       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//       return res.status(200).json({});
-//     }
-//     next();
-//   });
-// }
+function cors(app) {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Control-Type, Accept, Authorization',
+    );
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    next();
+  });
+}
+
+module.exports = cors;
+// const cors = require('cors');
 //
-// module.exports = cors;
-const cors = require('cors');
-
-const corsModule = (app) => {
-  app.use(
-    cors({
-      origin: process.env.CLIENT_HOST,
-      credentials: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-    }),
-  );
-};
-
-module.exports = corsModule;
+// const corsModule = (app) => {
+//   app.use(
+//     cors({
+//       origin: process.env.CLIENT_HOST,
+//       credentials: true,
+//       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//       preflightContinue: false,
+//     }),
+//   );
+// };
+//
+// module.exports = corsModule;
