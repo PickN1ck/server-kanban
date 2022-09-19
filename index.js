c7530975d4645423f41b2aca6efa59095597402f
PickN1ck <oopsparanoia@gmail.com>
2022-09-18 19:03:18 -0600

    CORS installed and fixed

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const errorHandler = require('./modules/core/errorHandler');
 const logger = require('./modules/core/logger');
-const cors = require('./modules/core/cors');
+
 const parseResponse = require('./modules/core/parseResponse');
 const routes = require('./modules/core/Routes');
 const dbConnection = require('./modules/core/dbConnection');
+const corsModule = require('./modules/core/cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 dbConnection();
 logger(app);
 parseResponse(app);
-cors(app);
+corsModule(app);
 routes(app);
 
 errorHandler(app);
