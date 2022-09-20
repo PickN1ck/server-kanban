c3770b5ee156d64e8800b836ecea09eead81c7e9
PickN1ck <oopsparanoia@gmail.com>
2022-09-19 20:25:53 -0600

    CORS like CRM

--- modules/core/cors.js
+++ modules/core/cors.js
@@ -17,17 +17,43 @@
 
 // const cors = require('cors');
 
+// const corsModule = (app) => {
+//   app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+//     );
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//     next();
+//   });
+// };
+//
+// module.exports = corsModule;
+
+const cors = require('cors');
+
 const corsModule = (app) => {
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    );
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    next();
-  });
+  app.use(
+    cors({
+      origin: process.env.CLIENT_HOST,
+      credentials: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+    }),
+  );
+
+  // app.use((req, res, next) => {
+  //     res.header('Access-Control-Allow-Origin', process.env.CLIENT_HOST);
+  //     res.header('Access-Control-Allow-Credentials', true);
+  //     res.header(
+  //       'Access-Control-Allow-Headers',
+  //       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  //     );
+  //     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+  //     next();
+  //   });
 };
 
 module.exports = corsModule;
