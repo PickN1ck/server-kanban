143e7fb81b99e56e5e0401b25030e1a742350d10
PickN1ck <oopsparanoia@gmail.com>
2022-09-19 18:19:09 -0600

    CORS allow all requests

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const routes = require('./modules/core/Routes');
 const dbConnection = require('./modules/core/dbConnection');
 
 const cors = require('./modules/core/cors');
+const corsDb = require('./modules/core/cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 dbConnection();
 logger(app);
 parseResponse(app);
-cors(app);
+corsDb(app);
 routes(app);
 
 errorHandler(app);
--- modules/core/cors.js
+++ modules/core/cors.js
@@ -1,19 +1,37 @@
-function cors(app) {
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Control-Type, Accept, Authorization',
-    );
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
-    }
-    next();
-  });
-}
+// function cors(app) {
+//   app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Control-Type, Accept, Authorization',
+//     );
+//     if (req.method === 'OPTIONS') {
+//       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//       return res.status(200).json({});
+//     }
+//     next();
+//   });
+// }
+//
+// module.exports = cors;
 
-module.exports = cors;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const corsDb = () => {
+  app.use(cors());
+
+  app.get('https://kanban-serv.herokuapp.com/card', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' });
+  });
+
+  app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80');
+  });
+};
+
+module.exports = corsDb;
 // const cors = require('cors');
 //
 // const corsModule = (app) => {
